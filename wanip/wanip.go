4ee64a23dbfac1b616e462b06e2ea1d7ec412265
Björn <41452212+bjoern621@users.noreply.github.com>
2025-03-06 21:01:00 +0100

    Added GetIPv6PrefixFromFritzBox

--- wanip/wanip.go
+++ wanip/wanip.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/xml"
 	"fmt"
+	"io"
 	"net/http"
 
 	"bjoernblessin.de/gorkbunddns/util/assert"
@@ -53,7 +54,7 @@ func GetFromFritzBox(ipProtocol string) (string, error) {
 
 	resp, err := (&http.Client{}).Do(request)
 	if err != nil {
-		return "", fmt.Errorf("error sending request %w", err)
+		return "", fmt.Errorf("Error sending request %w", err)
 	}
 	defer resp.Body.Close()
 
@@ -62,7 +63,7 @@ func GetFromFritzBox(ipProtocol string) (string, error) {
 
 		err = xml.NewDecoder(resp.Body).Decode(&response)
 		if err != nil {
-			return "", fmt.Errorf("couldn't parse XML %w", err)
+			return "", fmt.Errorf("Couldn't parse XML %w", err)
 		}
 
 		return response.Body.GetExternalIPAddressResponse.NewExternalIPAddress, nil
@@ -71,9 +72,54 @@ func GetFromFritzBox(ipProtocol string) (string, error) {
 
 		err = xml.NewDecoder(resp.Body).Decode(&response)
 		if err != nil {
-			return "", fmt.Errorf("couldn't parse XML %w", err)
+			return "", fmt.Errorf("Couldn't parse XML %w", err)
 		}
 
 		return response.Body.X_AVM_DE_GetExternalIPv6AddressResponse.NewExternalIPv6Address, nil
 	}
+}
+
+type _IPv6PrefixResponseEnvelope struct {
+	XMLName xml.Name `xml:"Envelope"`
+	Body    struct {
+		X_AVM_DE_GetIPv6PrefixResponse struct {
+			NewIPv6Prefix string `xml:"NewIPv6Prefix"`
+		} `xml:"X_AVM_DE_GetIPv6PrefixResponse"`
+	} `xml:"Body"`
+}
+
+// GetIPv6PrefixFromFritzBox sends a TR-064 SOAP request to the FRITZ!Box to retrieve the IPv6 prefix for the local network.
+// The prefix is in the form of "2001:db8:1234:5678::".
+func GetIPv6PrefixFromFritzBox() (string, error) {
+	soapRequest := `<?xml version="1.0" encoding="utf-8"?>
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:wan="urn:schemas-upnp-org:service:WANIPConnection:1">
+       <soapenv:Header/>
+       <soapenv:Body>
+          <wan:X_AVM_DE_GetIPv6Prefix/>
+       </soapenv:Body>
+    </soapenv:Envelope>`
+
+	request, err := http.NewRequest("POST", "http://fritz.box:49000/igdupnp/control/WANIPConn1", bytes.NewBuffer([]byte(soapRequest)))
+	assert.IsNil(err)
+
+	request.Header.Set("Content-Type", "text/xml; charset=utf-8")
+	request.Header.Set("SOAPACTION", "urn:schemas-upnp-org:service:WANIPConnection:1#X_AVM_DE_GetIPv6Prefix")
+
+	resp, err := (&http.Client{}).Do(request)
+	if err != nil {
+		return "", fmt.Errorf("Error sending request %w", err)
+	}
+	defer resp.Body.Close()
+
+	value, _ := io.ReadAll(resp.Body)
+	fmt.Printf("%s\n", value)
+
+	var response _IPv6PrefixResponseEnvelope
+
+	err = xml.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return "", fmt.Errorf("Couldn't parse XML %w", err)
+	}
+
+	return response.Body.X_AVM_DE_GetIPv6PrefixResponse.NewIPv6Prefix, nil
 }
